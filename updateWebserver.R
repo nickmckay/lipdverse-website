73b1c3c77e33e258d522e3fbcb36eb1d84b68b55
Nick McKay <nickmckay@users.noreply.github.com>
2023-03-28 12:31:17 -0700

    been awhile

--- updateWebserver.R
+++ updateWebserver.R
@@ -1,7 +1,15 @@
+
 unlink("~/GitHub/lipdverse-website/public/",recursive = TRUE)
+setwd("~/GitHub/lipdverse-website")
 blogdown::build_site()
 system("cp -r ~/GitHub/lipdverse-website/public/ ~/Dropbox/lipdverse/html")
 Sys.sleep(3)
+#replace blogs with articles
+bi <- readr::read_file("~/Dropbox/lipdverse/html/blog/index.html")
+
+bir <- stringr::str_replace_all(bi,"Blogs","Articles")
+readr::write_file(bir,"~/Dropbox/lipdverse/html/blog/index.html")
+
 # upload root level files:
 system("rsync -vd --delete /Users/nicholas/Dropbox/lipdverse/html/ npm4@linux.cefns.nau.edu:/www/cefns.nau.edu/seses/lipdverse")
 
